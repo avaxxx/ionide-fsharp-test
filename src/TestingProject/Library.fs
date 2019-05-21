11987698ed875575991a909b914ed1cc077c3665
Pavol Decky <avaxxx@gmail.com>
2019-05-21 13:54:09 +0200

    Remove PAKET replace with NUGET in testing project

--- src/TestingProject/Library.fs
+++ src/TestingProject/Library.fs
@@ -2,6 +2,8 @@
 
 open Xunit
 open FsUnit.Xunit
+open Argu
+open MathNet.Numerics.LinearAlgebra
 
 module Tests =
     let (|EndOfGame|IncompleteStrike|Strike|Normal|Other|) (l, frame) =
@@ -11,21 +13,32 @@ module Tests =
         | 10::s::n::tail               -> Strike(10+s+n, s::n::tail)
         |  f::s::n::tail when f+s = 10 -> Normal(f+s+n,  n::tail)
         |  f::s::n::tail               -> Normal(f+s,    n::tail)
-        | ls                           -> Other(List.fold (+) 0 ls)
+        | ls                           -> Other(List.sum ls)
 
     let scoreBowls bowls =
-        let rec scoreBowls' frame l current_score =
+        let rec scoreBowls' frame l currentscore =
             let nextframe = scoreBowls' (frame+1)
             match (l, frame) with
-            | EndOfGame(score)        -> current_score + score
-            | IncompleteStrike(score) -> current_score + score
-            | Strike(score, l)        -> nextframe l (current_score + score)
-            | Normal(score, l)        -> nextframe l (current_score + score)
-            | Other(score)            -> current_score + score
+            | EndOfGame(score)        -> currentscore + score
+            | IncompleteStrike(score) -> currentscore + score
+            | Strike(score, l)        -> nextframe l (currentscore + score)
+            | Normal(score, l)        -> nextframe l (currentscore + score)
+            | Other(score)            -> currentscore + score
         scoreBowls' 1 bowls 0
+
+    
+
 
     [<Fact>]
     let ``My test`` () =
+        let i = 10 
+        let m = matrix [[ 1.0; 2.0 ]
+                        [ 3.0; 4.0 ]]
+        let m' = m.Inverse()
+        Assert.True(true)
+
+    [<Fact>]
+    let ``My test 2`` () =
         Assert.True(true)
 
     [<Fact>]
